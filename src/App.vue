f54d92579f31ccfc92bb43183297add3c8a06b91
xiaoxin179 <97381599+xiaoxin179@users.noreply.github.com>
2024-01-17 21:45:47 +0800

    最原始模板

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>最初始</h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeViews from '@/views/HomeViews.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeViews
+    },
+  ]
+})
+
+export default router
